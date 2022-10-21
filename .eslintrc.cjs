ffcae3a62013ada7adb6f8f562d1620303d6503e
ukyi <devcat.js@gmail.com>
2022-10-21 13:54:16 +0900

    🔥 import/first rule 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,119 +1,123 @@
-const { defineConfig } = require('eslint-define-config');
+const { defineConfig } = require("eslint-define-config");
 
 module.exports = defineConfig({
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript', 'plugin:vue/vue3-recommended'],
-  plugins: ['import'],
-  parser: 'vue-eslint-parser',
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:vue/vue3-recommended",
+  ],
+  plugins: ["import"],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
     ecmaVersion: 2021,
   },
   rules: {
     eqeqeq: [
-      'warn',
-      'always',
+      "warn",
+      "always",
       {
-        null: 'never',
+        null: "never",
       },
     ],
-    'no-debugger': ['error'],
-    'no-empty': [
-      'warn',
+    "no-debugger": ["error"],
+    "no-empty": [
+      "warn",
       {
         allowEmptyCatch: true,
       },
     ],
-    'no-process-exit': 'off',
-    'no-useless-escape': 'off',
-    'no-var': 'error',
-    'no-trailing-spaces': 'error',
-    'no-multiple-empty-lines': [
-      'error',
+    "no-process-exit": "off",
+    "no-useless-escape": "off",
+    "no-var": "error",
+    "no-trailing-spaces": "error",
+    "no-multiple-empty-lines": [
+      "error",
       {
         max: 1,
         maxEOF: 0,
       },
     ],
-    'arrow-parens': ['error', 'as-needed'],
-    'arrow-body-style': ['error', 'as-needed'],
-    'prefer-const': [
-      'warn',
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-body-style": ["error", "as-needed"],
+    "prefer-const": [
+      "warn",
       {
-        destructuring: 'all',
+        destructuring: "all",
       },
     ],
-    semi: 'error',
-    quotes: ['error', 'single'],
-    '@typescript-eslint/explicit-module-boundary-types': [
-      'error',
+    semi: "error",
+    quotes: ["error", "single"],
+    "@typescript-eslint/explicit-module-boundary-types": [
+      "error",
       {
         allowArgumentsExplicitlyTypedAsAny: true,
       },
     ],
-    '@typescript-eslint/no-empty-function': [
-      'error',
+    "@typescript-eslint/no-empty-function": [
+      "error",
       {
-        allow: ['arrowFunctions'],
+        allow: ["arrowFunctions"],
       },
     ],
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
       {
-        prefer: 'type-imports',
+        prefer: "type-imports",
       },
     ],
-    'import/no-duplicates': 'error',
-    'import/first': 'error',
-    'import/newline-after-import': 'error',
-    'import/order': [
-      'error',
+    "import/no-duplicates": "error",
+    "import/newline-after-import": "error",
+    "import/order": [
+      "error",
       {
-        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        groups: ["builtin", "external", ["parent", "sibling"], "index"],
         pathGroups: [
           {
-            pattern: '@/**',
-            group: 'external',
-            position: 'after',
+            pattern: "@/**",
+            group: "external",
+            position: "after",
           },
         ],
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: true,
         },
-        'newlines-between': 'never',
+        "newlines-between": "never",
       },
     ],
-    'sort-imports': [
-      'error',
+    "sort-imports": [
+      "error",
       {
         ignoreCase: true,
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
         allowSeparatedGroups: false,
       },
     ],
-    'vue/html-self-closing': [
-      'error',
+    "vue/html-self-closing": [
+      "error",
       {
         html: {
-          void: 'always',
-          normal: 'always',
-          component: 'always',
+          void: "always",
+          normal: "always",
+          component: "always",
         },
-        svg: 'always',
-        math: 'always',
+        svg: "always",
+        math: "always",
       },
     ],
-    'vue/max-attributes-per-line': [
-      'error',
+    "vue/max-attributes-per-line": [
+      "error",
       {
         singleline: {
           max: 10,
